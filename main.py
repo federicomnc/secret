2149f063175b15989475894b9ac4a61f6029364a
federicomnc <federico.mnc@gmail.com>
2021-02-08 16:55:55 +0100

    Added nome from ignora.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from secret import password
+from ignora import nome
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -14,5 +15,6 @@ if __name__ == '__main__':
     print_hi('Federico')
     print('How are you?')
     print(password)
+    print(nome)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
